4b3972ead12cacb722a272f5a3e2b8969cd310fd
ManalWardi <116381871+ManalWardi@users.noreply.github.com>
2023-05-24 00:36:57 +0100

    version2
    create table participant
    affichage participants dans fenetre participants (principalUser)
    formulaire ajout participants dans gestion des participants (principale)

--- gestion_participant.php
+++ gestion_participant.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <title>Competition d'Arts</title>
+  <title>Gestion Participants</title>
 </head>
  <body>
  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -16,13 +16,13 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">Accueil</a>
+          <a class="nav-link" href="principale.php">Accueil</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Gestion des Catégories</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="gestion_participant.php">Gestion des Participants</a>
+          <a class="nav-link" href="#">Gestion des Participants</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Gestion des Œuvres</a>
@@ -48,37 +48,81 @@
       </ul>
     </div>
   </nav>
-  <div class="container mt-4">
- <!-- tester si l'utilisateur est connecté -->
+
+  <?php
+
+$host='localhost';
+$dbname='login';
+$username='root';
+$password='';
+try{
+    $db = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
+    //Definir mode d'erreur de PDO a Exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e){
+    die('Erreur:'.$e->getMessage());
+}
+?>
+<div class="col-md-6">
+    <br>
+        <h3>Ajout de participant:</h3><br>
+<form method="POST" >
+<div class="form-group">
+<label>Id:</label>
+<input type="text" class="form-control" name="idPar">
+</div>
+<div class="form-group">
+<label>Nom:</label>
+<input type="text" class="form-control" name="nomPar">
+</div>
+<div class="form-group">
+<label>Prenom:</label>
+<input type="text" class="form-control" name="prenomPar">
+</div>
+<div class="form-group">
+<label>Age:</label>
+<input type="number" class="form-control" name="agePar">
+</div>
+<div class="form-group">
+<label>Email:</label>
+<input type="email" class="form-control" name="emailPar">
+</div>
+<div class="form-group">
+<label>Talent:</label>
+<input type="text" class="form-control" name="talent">
+</div>
+
+<button type="submit" class="btn btn-primary">Ajouter</button>
+
+</form>
+</div>
+
+<?php
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $id=$_POST['idPar'];
+    $nom=$_POST['nomPar'];
+    $prenom=$_POST['prenomPar'];
+    $age=$_POST['agePar'];
+    $email=$_POST['emailPar'];
+    $talent=$_POST['talent'];
+   
+
+    $sth=$db->prepare("INSERT INTO participant(id,nom,prenom,email,age,talent) VALUES (:id,:nom,:prenom,:email,:age,:talent)");
+    $sth->bindValue(':id',$id);
+    $sth->bindValue(':nom',$nom);
+    $sth->bindValue(':prenom',$prenom);
+    $sth->bindValue(':email',$email);
+    $sth->bindValue(':age',$age);
+    $sth->bindValue(':talent',$talent);
+
+    $sth->execute();
 
 
- 
- <!-- tester si l'utilisateur est connecté -->
- <?php
- session_start();
- if(isset($_GET['deconnexion']))
- { 
- if($_GET['deconnexion']==true)
- { 
- session_unset();
- header("location:login.php");
- }
- }
- else if($_SESSION['username'] !== ""){
- $user = $_SESSION['username'];
- // afficher un message
- echo "<br>Bonjour $user, vous êtes connectés";
- }
- ?>
- 
- <div class="container mt-4">
-    <h3>Bienvenue sur la page de gestion de compétition d'arts</h3>
-    <p>Dans cette page vous pouvez gerer les participants, les jury, le resultat et les prix de la competition.</p>
-  </div>
- </div>
+}
+?>
 
-
- <!-- ... Fin du code HTML ... -->
+  <!-- ... Fin du code HTML ... -->
         
 
  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
--- participant.php
+++ participant.php
@@ -16,7 +16,7 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">Accueil</a>
+          <a class="nav-link" href="principalUser.php">Accueil</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Compétition</a>
@@ -25,7 +25,7 @@
           <a class="nav-link" href="#">Catégories</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="participant.php">Participants</a>
+          <a class="nav-link" href="#">Participants</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Œuvres</a>
@@ -46,26 +46,62 @@
           <a class="nav-link" href="#">Contact</a>
         </li>
         <li class="nav-item">
+          <a class="nav-link" href="#">Formulaire candidature</a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link" href="login.php">login Admin</a>
         </li>
       </ul>
     </div>
   </nav>
 
-  <div class="container mt-4">
-    <h1>Bienvenue sur la compétition d'arts</h1>
-    <p>C est une competition d arts qui regroupe des talents </p>
-  </div>
+  <?php
+
+$host='localhost';
+$dbname='login';
+$username='root';
+$password='';
+try{
+    $db = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
+    //Definir mode d'erreur de PDO a Exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e){
+    die('Erreur:'.$e->getMessage());
+}
+?>
+<?php $requete=$db->query('SELECT * FROM participant');?>
+<p id="p1">
+
+
+  <div class="col-md-6">
+        <h3>Participants: </h3><br>
+        <?php while($row = $requete->fetch(PDO::FETCH_ASSOC)) :  ?>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Participant  <?php  echo htmlspecialchars($row['prenom']).' '.htmlspecialchars($row['nom']); ?> </h5>
+            
+            <p class="card-text"><b>Informations sur le participant:</b>
+              <label> Je suis <?php echo  htmlspecialchars($row['prenom']).' '.htmlspecialchars($row['nom']); ?>. Mon id est: <?php echo htmlspecialchars($row['id']); ?>. J'ai <?php echo htmlspecialchars($row['age']); ?> et mon talent est:
+  <?php echo htmlspecialchars($row['talent']); ?></label>
+            </p>
+            <a href="#" class="btn btn-primary">Voir le profil</a>
+          </div>
+        </div>
+      
+   
+   <br><br> <?php endwhile; ?>
+   </div>
+</p>
+
 
    <!-- ... Fin du code HTML ... -->
         
 
-   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+ <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.6.0/js/bootstrap.min.js"></script>
  
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-  
-</body>
+ </body>
 </html>
