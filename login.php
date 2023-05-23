b68aa60f49309d4475269a5144dfd155ce5d22b2
Masbah Aya <123211272+AyaMasbah@users.noreply.github.com>
2023-05-23 19:41:44 +0100

    added min files

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>LOGIN ADMIN</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<br>
+<br>
+<div class="container">
+  <h2>Login Admin</h2>
+  <form action="verification.php" method="POST">
+    <div class="form-group">
+      <label for="username">Username:</label>
+      <input type="text" class="form-control" id="email" placeholder="Enter username" name="username" required>
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="password" required>
+    </div>
+    <div class="form-group form-check">
+      <label class="form-check-label">
+        <input class="form-check-input" type="checkbox" name="remember"> Remember me
+      </label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+    <a href="principalUser.php" class="btn btn-primary">Retour au site d acceuil</a>
+
+  </form>
+</div>
+
+
+
+
+
+
+</body>
+</html>
---
+++ principalUser.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title>Competition d'Arts</title>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Competition d'Arts</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Compétition</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="categorie.html">Catégories</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Participants</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Œuvres</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Jury</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Prix</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Résultats</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Commentaires</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="login.php">login Admin</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container mt-4">
+    <h1>Bienvenue sur la compétition d'arts</h1>
+    <p>C est une competition d arts qui regroupe des talents </p>
+  </div>
+
+   <!-- ... Fin du code HTML ... -->
+        
+
+   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.6.0/js/bootstrap.min.js"></script>
+ 
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+  
+</body>
+</html>
